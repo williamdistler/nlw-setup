2dcb35de44a987e98b48502d8a558967eb3d9bfb
williamsnow <will.distlerneves@gmail.com>
2023-01-22 23:00:47 -0300

    FIX: adicao de muitos dias

--- script.js
+++ script.js
@@ -18,28 +18,6 @@ function add() {
   alert("Adicionado com sucesso ✔️")
 }
 
-nlwSetup.addDay("01/01")
-nlwSetup.addDay("02/01")
-nlwSetup.addDay("03/01")
-nlwSetup.addDay("04/01")
-nlwSetup.addDay("05/01")
-nlwSetup.addDay("06/01")
-nlwSetup.addDay("07/01")
-nlwSetup.addDay("08/01")
-nlwSetup.addDay("09/01")
-nlwSetup.addDay("10/01")
-nlwSetup.addDay("11/01")
-nlwSetup.addDay("12/01")
-nlwSetup.addDay("13/01")
-nlwSetup.addDay("14/01")
-nlwSetup.addDay("15/01")
-nlwSetup.addDay("16/01")
-nlwSetup.addDay("17/01")
-nlwSetup.addDay("18/01")
-nlwSetup.addDay("19/01")
-nlwSetup.addDay("20/01")
-nlwSetup.addDay("21/01")
-
 function save() {
   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
